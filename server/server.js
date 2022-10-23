662c0fb39009174ef88df314b1b23b0c1ec5bdb4
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-23 13:40:12 +0200

    Step 17 - MongoDB connection (WiP)

--- server/server.js
+++ server/server.js
@@ -2,10 +2,26 @@ import express from "express";
 import * as path from "path";
 import {MoviesApi} from "./moviesApi.js";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const mongodburl = process.env.MONGODB_URL;
+if(mongodburl) {
+    const client = new MongoClient(mongodburl);
+    client
+        .connect()
+        .then((conn) =>
+        app.use("/api/movies",
+            MovieApi(conn.db(process.env.MONGODB_DATABASE || "movie-reference "))
+        )
+    );
+}
+
 app.use("/api/movies", MoviesApi());
 
 app.use(express.static("../client/dist"));
