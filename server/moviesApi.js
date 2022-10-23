53589f8f9428a5072cb81ddeb805d2f141db6c9b
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-23 13:17:13 +0200

    Step 13 - Adding a new movie - server

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -2,13 +2,27 @@ import express from "express";
 
 export const MoviesApi = express.Router();
 
+// temporary local storage
+const movies = [
+    {
+        title : "Plan 9",
+        year: "1957",
+        plot: "confused",
+    },
+    {
+        title: "Dune",
+        year: "2021",
+        plot: "Arrakis, Dune, desert planet"
+    },
+
+];
+
 MoviesApi.get("/", (req, res) => {
-   res.json([
-       {
-           title : "Plan 9",
-       },
-       {
-         title: "Dune",
-       },
-   ]);
+   res.json(movies);
+});
+
+MoviesApi.post("/", (req, res) => {
+    const { title, plot, year } = req.body;
+    movies.push( { title, plot, year });
+    res.sendStatus(204);
 });
