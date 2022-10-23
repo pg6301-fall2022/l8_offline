0288fafdda0d7b54a4ff3228ff71e13c693dd6dd
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-23 12:18:49 +0200

    Step 11 - Fetch and useLoader - part 3 - MoviesAPI

---
+++ server/moviesApi.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+export const MoviesApi = express.Router();
+
+MoviesApi.get("/", (req, res) => {
+   res.json([
+       {
+           title : "Plan 9",
+       },
+       {
+         title: "Dune",
+       },
+   ]);
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import * as path from "path";
+import {MoviesApi} from "./moviesApi.js";
 
 const app = express();
 
-app.use(express.static("../client/dist"));
+app.use("/api/movies", MoviesApi);
 
+app.use(express.static("../client/dist"));
 app.use((req, res, next) => {
    if(req.method === "GET" && !req.path.startsWith("/api")){
        return res.sendFile(path.resolve("../client/dist/index.html"));
