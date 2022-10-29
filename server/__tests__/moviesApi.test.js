322bc62cca666034a86c4976c6315a2b2c5bded1
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-29 15:50:10 +0200

    Step 8 - Basic backend test

--- server/__tests__/moviesApi.test.js
+++ server/__tests__/moviesApi.test.js
@@ -2,13 +2,26 @@ import express from "express";
 import request from "supertest";
 import bodyParser from "body-parser";
 import { MoviesApi } from "../moviesApi.js";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 
 const app = express();
 app.use(bodyParser.json());
+let mongoClient;
 
-app.use("/api/movies", MoviesApi);
+beforeAll(async () => {
+    dotenv.config();
+    mongoClient = new MongoClient(process.env.MONGODB_URL);
+    await mongoClient.connect();
+    const database = mongoClient.db("unit_tests");
+    await database.collection("movies").deleteMany({});
+    app.use("/api/movies", MoviesApi(database));
+});
+
+afterAll(() => {
+    mongoClient.close();
+});
 
 describe("movies api test suite", () => {
     it("does something", async () => {
